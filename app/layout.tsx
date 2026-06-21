95c602c3ab7eb5782b93a4d95897b24102a48cfc
notkunz <fkunzy@gmail.com>
2026-06-21 13:37:32 +0100

    testing maintainance 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,6 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        {children}
         <MaintenanceCheck>{children}</MaintenanceCheck>
       </body>
     </html>
